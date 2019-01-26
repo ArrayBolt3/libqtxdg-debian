441546a21fb11c727d3aafd6f721fc4776a184b6
Alf Gaida <agaida@siduction.org>
2019-01-26 12:26:42 +0100

    Adding upstream version 3.3.0.
    Signed-off-by: Alf Gaida <agaida@siduction.org>

--- src/xdgiconloader/xdgiconloader.cpp
+++ src/xdgiconloader/xdgiconloader.cpp
@@ -136,7 +136,7 @@ private:
             | m_data[offset+1] << 16 | m_data[offset] << 24;
     }
 };
-Q_GLOBAL_STATIC(QFileSystemWatcher, gtkCachesWatcher);
+Q_GLOBAL_STATIC(QFileSystemWatcher, gtkCachesWatcher)
 
 
 QIconCacheGtkReader::QIconCacheGtkReader(const QString &dirName)
@@ -405,7 +405,7 @@ QThemeIconInfo XdgIconLoader::findIconHelper(const QString &themeName,
     QStringRef iconNameFallback(&iconName);
 
     // Iterate through all icon's fallbacks in current theme
-    while (info.entries.isEmpty()) {
+    if (info.entries.isEmpty()) {
         const QString svgIconName = iconNameFallback + svgext;
         const QString pngIconName = iconNameFallback + pngext;
         const QString xpmIconName = iconNameFallback + xpmext;
@@ -470,8 +470,6 @@ QThemeIconInfo XdgIconLoader::findIconHelper(const QString &themeName,
 
         if (!info.entries.isEmpty())
             info.iconName = iconNameFallback.toString();
-
-        break;
     }
 
     if (info.entries.isEmpty()) {
@@ -631,7 +629,8 @@ void XdgIconLoaderEngine::paint(QPainter *painter, const QRect &rect,
 #if defined(Q_DEAD_CODE_FROM_QT4_MAC)
     pixmapSize *= qt_mac_get_scalefactor();
 #endif
-    painter->drawPixmap(rect, pixmap(pixmapSize, mode, state));
+    const qreal dpr = painter->device()->devicePixelRatioF();
+    painter->drawPixmap(rect, pixmap(pixmapSize * dpr, mode, state));
 }
 
 /*
@@ -823,6 +822,11 @@ QPixmap ScalableEntry::pixmap(const QSize &size, QIcon::Mode mode, QIcon::State 
     return svgIcon.pixmap(size, mode, state);
 }
 
+static const QString STYLE = QStringLiteral("\n.ColorScheme-Text, .ColorScheme-NeutralText {color:%1;}\
+\n.ColorScheme-Background {color:%2;}\
+\n.ColorScheme-Highlight {color:%3;}");
+// Note: Qt palette does not have any colors for positive/negative text
+// .ColorScheme-PositiveText,ColorScheme-NegativeText {color:%4;}
 
 QPixmap ScalableFollowsColorEntry::pixmap(const QSize &size, QIcon::Mode mode, QIcon::State state)
 {
@@ -841,8 +845,8 @@ QPixmap ScalableFollowsColorEntry::pixmap(const QSize &size, QIcon::Mode mode, Q
             const QPalette pal = qApp->palette();
             // Note: indexes are assembled as in qtsvg (QSvgIconEnginePrivate::hashKey())
             QMap<int, QString> style_sheets;
-            style_sheets[(QIcon::Normal<<4)|QIcon::Off] = QStringLiteral(".ColorScheme-Text{color:%1;}").arg(pal.windowText().color().name());
-            style_sheets[(QIcon::Selected<<4)|QIcon::Off] = QStringLiteral(".ColorScheme-Text{color:%1;}").arg(pal.highlightedText().color().name());
+            style_sheets[(QIcon::Normal<<4)|QIcon::Off] = STYLE.arg(pal.windowText().color().name()).arg(pal.window().color().name()).arg(pal.highlight().color().name());
+            style_sheets[(QIcon::Selected<<4)|QIcon::Off] = STYLE.arg(pal.highlightedText().color().name()).arg(pal.highlight().color().name()).arg(pal.highlightedText().color().name());
             QMap<int, QSharedPointer<QXmlStreamWriter> > writers;
             for (auto i = style_sheets.cbegin(); i != style_sheets.cend(); ++i)
             {
@@ -856,16 +860,27 @@ QPixmap ScalableFollowsColorEntry::pixmap(const QSize &size, QIcon::Mode mode, Q
                         && xmlReader.qualifiedName() == QLatin1String("style")
                         && xmlReader.attributes().value(QLatin1String("id")) == QLatin1String("current-color-scheme"))
                 {
+                    const auto attribs = xmlReader.attributes();
+                    // store original data/text of the <style> element
+                    QString original_data;
+                    while (xmlReader.tokenType() != QXmlStreamReader::EndElement)
+                    {
+                        if (xmlReader.tokenType() == QXmlStreamReader::Characters)
+                            original_data += xmlReader.text();
+                        xmlReader.readNext();
+                    }
                     for (auto i = style_sheets.cbegin(); i != style_sheets.cend(); ++i)
                     {
                         QXmlStreamWriter & writer = *writers[i.key()];
                         writer.writeStartElement(QLatin1String("style"));
-                        writer.writeAttributes(xmlReader.attributes());
+                        writer.writeAttributes(attribs);
+                        // Note: We're writting the original style text to leave
+                        // there "defaults" for unknown/unsupported classes.
+                        // Then appending our "overrides"
+                        writer.writeCharacters(original_data);
                         writer.writeCharacters(*i);
                         writer.writeEndElement();
                     }
-                    while (xmlReader.tokenType() != QXmlStreamReader::EndElement)
-                        xmlReader.readNext();
                 } else if (xmlReader.tokenType() != QXmlStreamReader::Invalid)
                 {
                     for (auto i = style_sheets.cbegin(); i != style_sheets.cend(); ++i)
